c10150f4c022872d03ce95964de84235a7a54b7d
Manasi <mgkhandekar@gmail.com>
2020-07-25 10:06:57 +0530

    Updates

---
+++ precache-manifest.0e10f17c1c31b5eaefbb521a4d400398.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82202f59b85ad8043708fef2e83dd71",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c97849fac95840b8b21e",
+    "url": "/my-app/static/css/main.509fee1c.chunk.css"
+  },
+  {
+    "revision": "2da9d4a0d58b9ce1c88a",
+    "url": "/my-app/static/js/2.d0348093.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.d0348093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97849fac95840b8b21e",
+    "url": "/my-app/static/js/main.c9570088.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b3c5e867c63d65be5926df362c7543fa",
+    "url": "/my-app/static/media/mee.b3c5e867.JPG"
+  }
+]);
